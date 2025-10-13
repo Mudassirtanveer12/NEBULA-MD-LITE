94a9e9ab9c45bb61b729e2afe04a03ae28e31199
Ridz Coder <148397393+ridz-coder01@users.noreply.github.com>
2025-10-13 20:06:12 +0300

    Update and rename config.js to config.cjs

--- config.cjs
+++ config.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "Nebula~6uhCBCqR#1KEL7UbBUrDwAae5kCpPhftGUzW6dPPMADfFW0G4jzw",
   PREFIX: process.env.PREFIX || '.',
   BOT_NAME: process.env.BOT_NAME || "NEBULA MD LITE",
   BOT: process.env.BOT || "hello 👋",
